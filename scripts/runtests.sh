9a31699fdf03a1e5dff499e4b176c5f597ed6396
José Fernando Cordova <josefernando.c@geopagos.com>
2018-06-17 03:49:52 -0300

    refactoring

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
 # Make sure starter fixtures can load successfully and all tests pass.
-docker exec dockerlaravelapidev_php_1 php artisan key:generate
+docker exec dockerlaravelapidev_php_1 composer install && cp .env.example .env && php artisan key:generate
+docker exec dockerlaravelapidev_php_1 php artisan migrate
 
 
 
