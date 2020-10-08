c7e35f824c9e05678a943c158694c83d6310c069
José Fernando Cordova <jfernancordova@gmail.com>
2020-10-08 09:59:00 -0300

    HF | Fix runtests.sh for travis

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 # Make sure starter fixtures can load successfully and all tests pass.
-docker-compose exec php vendor/bin/phpunit
+docker exec docker-laravel-api-dev_php_1 vendor/bin/phpunit
 
 
 
